d2c4c64df71e0f26867cf36da7e7e6c2050c04ad
NakkaNithish27 <nakkanithish27@gmail.com>
2024-02-18 23:57:33 +0530

    upto synchronized keyword

--- Demo Folder/Demo.java
+++ Demo Folder/Demo.java
@@ -1,16 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 
-class Demo {
-	public static void main(String[] args) throws IOException {
-		InputStreamReader in = new InputStreamReader(System.in);
-		BufferedReader bf = new BufferedReader(in);
-		int num = Integer.parseInt(bf.readLine());
-		System.out.println(num);
-
-		bf.close();//closing the resource
+class Count {
+	int counter;
+	public synchronized void increment() {
+		counter++;
 	}
 }
 
+class Demo {
+	public static void main(String[] args) throws InterruptedException {
+		Count c = new Count();
+		Runnable obj1 = () -> {
+			for (int i = 0; i < 1000; i++) {
+				c.increment();
+			}
 
+		};
+
+		Runnable obj2 = () -> {
+			for (int i = 0; i < 1000; i++) {
+				c.increment();
+			}
+		};
+		Thread t1 = new Thread(obj1);
+		Thread t2 = new Thread(obj2);
+		t1.start();
+		t2.start();
+
+		/*If we don't mention this, the main thread will execute the println statement first
+		as it is free, we are asking main thread to wait while t1,t2 finish*/
+		t1.join();
+		t2.join();
+
+		System.out.println(c.counter);
+
+	}
+}
