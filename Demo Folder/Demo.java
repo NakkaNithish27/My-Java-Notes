32c187d6a46998575962e8cad02cf26be20287df
NakkaNithish27 <nakkanithish27@gmail.com>
2024-03-08 00:16:03 +0530

    added Servelet&Jsp notes file

--- Demo Folder/Demo.java
+++ Demo Folder/Demo.java
@@ -1,30 +1,14 @@
-import java.util.List;
-import java.util.stream.Stream;
-import java.util.Arrays;
+
 
 class Demo {
 	public static void main(String[] args) {
-		List <Integer> nums = Arrays.asList(4, 7, 8, 3, 9, 2);
-		int result = nums.stream()
-		             .filter(n -> n % 2 == 0)  //takes object of Predicate interface
-		             .map(n->n * 2) //takes object of Function interface
-		             .reduce(0, (c, e)->c + e); /*0 is the intitial value, c is carry and e is the element*/
-		System.out.println(result);
-
-		/*To get sorted values*/
-		Stream<Integer> sortedValues = nums.stream()
-		                               .filter(n -> n % 2 == 0)
-		                               .map(n->n * 2)
-		                               .sorted();
-		sortedValues.forEach(n-> System.out.print(n));
-		System.out.println();
-
-		/*What if want to do filtering with multiple threads, we can use parallelStream*/
-		/*use only if you want to filter, don't use parallel stream if you want to sort the stream values*/
-		Stream<Integer> ps = nums.parallelStream()
-		                     .filter(n->n % 2 == 0);
-		ps.forEach(e->System.out.print(e));
-
+		int i = 12345;
+		int j = 0;
+		while (i != 0) {
+			j = i % 10;
+			i = i / 10;
+			System.out.println(j);
+		}
 	}
 }
 
---
+++ Servlet&JspNotes.java
@@ -0,0 +1,6 @@
+/*Servelet is a java file that do all the processing and generates a static html page as a response
+  Apache Tomcat(web container) server processes these servelet file*/
+
+//Download eclipse EE>download and configure tomcat>create a new dynamic web application
+
+//In web.xml (deployment descriptor we do all the servlet mapping) 
